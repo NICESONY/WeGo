b128ef2c5ea81c2d23de0459b4aa33c6ff6caf1d
NICESONY <songunhee5426@gmail.com>
2025-04-12 06:24:55 +0000

    publisher

--- catkin_ws/src/hello_ros/scripts/hello.py
+++ catkin_ws/src/hello_ros/scripts/hello.py
@@ -1,8 +1,19 @@
+import rospy
+from std_msgs.msg import String
+
 
 def main():
-    print("hi")
+    rospy.init_node('hello', anonymous=True)
+    pub = rospy.Publisher('message', String, queue_size=10)
+    data = String()
+    i=0
+    data.data = f"hellom, ROS! netic {i}"
+    rate = rospy.Rate(3)
+    while True:
+        pub.publish(data)
+        print("hello, ROS1 noetic!!")
+        rate.sleep()
+        i += 1
 
-
-
-if __name__ =="__main__" :
+if __name__ == "__main__":
     main()
