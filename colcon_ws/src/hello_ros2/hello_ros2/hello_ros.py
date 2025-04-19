bae1ef74f700ef771116cf792a5261d51b1e0bf3
github <github>
2025-04-19 10:40:18 +0900

    update_04_19

--- colcon_ws/src/hello_ros2/hello_ros2/hello_ros.py
+++ colcon_ws/src/hello_ros2/hello_ros2/hello_ros.py
@@ -1,8 +1,20 @@
 import rclpy as rp
+from rclpy.node import Node
 
+
+def print_hello():
+    print("hello ROS2 Humble")
 
 def main():
-    print("hello ROS2 Humble")
+    rp.init()
+    node = Node("hello_ros")
+    node.create_timer(1, print_hello)
+    try :
+        rp.spin(node)
+    except KeyboardInterrupt:
+        node.destroy_node()
+    rp.spin(node)
+    
 
 if __name__ == "__main__" :
     main()
