9460865e7019d9a10db9c1162ed226a324d52f49
NICESONY <songunhee5426@gmail.com>
2025-04-26 17:18:35 +0900

    check ServiceServer

--- colcon_ws/src/hello_ros2/hello_ros2/simpleServiceServer.py
+++ colcon_ws/src/hello_ros2/hello_ros2/simpleServiceServer.py
@@ -1,10 +1,26 @@
 import rclpy as rp
 from rclpy.node import Node  # ros2 노드라는 클래스를 제공하므로 이걸로 클래스를 만들면 된다.
-from std_msgs.srv import SetBool
+from std_srvs.srv import SetBool 
+
 
 # 객체지향으로 만들기
 class Service_server(Node):
-    pass
+    def __init__(self):
+        super().__init__('simpleServiceServer')
+        self.srv = self.create_service(SetBool,"simpleServiceServer", self.callback)
+        self.bool = False
+
+    
+    def callback(self, 
+                 request : SetBool.Request , 
+                 response : SetBool.Response):
+        self.get_logger().info("Service server is called")
+        self.bool = request.data
+        response.message = '변경이 성공했습니다'
+        response.success = self.bool
+        return response
+    
+    
 
 
 def main():
@@ -15,7 +31,7 @@ def main():
     except KeyboardInterrupt :
         node.destroy_node()
     
-    
+
 
 
 
