3236b2f1c81b104bdc6eff571f701fff8eb1e379
NICESONY <songunhee5426@gmail.com>
2025-07-28 00:10:16 -0700

    make code that split the mode

--- son_wav_backup/src/stt_node/scripts/stt_listener.py
+++ son_wav_backup/src/stt_node/scripts/stt_listener.py
@@ -4,12 +4,23 @@
 import rospy
 from std_msgs.msg import String
 
-def callback(msg):
-    # 한글이 깨지지 않고 바로 출력됩니다.
-    print msg.data
+class STTListener(object):
+    def __init__(self):
+        rospy.init_node('stt_listener', anonymous=True)
+        rospy.loginfo("STT Listener started, waiting for messages on /stt_topic")
+        self.sub = rospy.Subscriber('/stt_topic', String, self.callback)
 
-if __name__ == '__main__':
-    rospy.init_node('stt_listener')
-    rospy.Subscriber('/stt_topic', String, callback)
+    def callback(self, msg):
+        # 1) print 로
+        # print msg.data
+
+        # 2) rospy.loginfo 로 (원하는 방식으로 하나만 쓰셔도 됩니다)
+        rospy.loginfo("Received STT text: %s", msg.data)
+
+def main() :
+    node = STTListener()
+    rospy.on_shutdown(node.shutdown)
     rospy.spin()
 
+if __name__ == '__main__':
+    main()
